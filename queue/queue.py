374932e1e140bd3d71b645a76a5621c20d7d4982
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-26 07:22:30 -0500

    After demo to Erik

--- queue/queue.py
+++ queue/queue.py
@@ -13,23 +13,24 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-from singly_linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
+        self.storage = []
     
     def __len__(self):
         return len(self.storage)
 
     def enqueue(self, value):
         self.size += 1
-        self.storage.add_to_tail(value)
+        self.storage.append(value)
 
     def dequeue(self):
         if (self.size > 0):
             self.size -= 1
-            return self.storage.remove_head()
+            returnValue = self.storage[0]
+            self.storage =self.storage[1:]
+            return returnValue
         else:
             return None
 
--- queue/queue1.py
+++ queue/queue1.py
@@ -13,24 +13,23 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+from singly_linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
     
     def __len__(self):
         return len(self.storage)
 
     def enqueue(self, value):
         self.size += 1
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if (self.size > 0):
             self.size -= 1
-            returnValue = self.storage[0]
-            self.storage =self.storage[1:]
-            return returnValue
+            return self.storage.remove_head()
         else:
             return None
 
